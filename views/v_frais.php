9eb9b0121379bd2fb0e3d36c450d484d84a9c80d
Abdenour ALLAOUAT <abdenour.allaouat2@gmail.com>
2024-03-08 12:04:03 +0100

    etoil

--- views/v_frais.php
+++ views/v_frais.php
@@ -1,26 +1,27 @@
 <?php require 'helpers/help.php'; ?>
-
-<h1>Bienvenue <?php echo $_SESSION['nom']." " .$_SESSION['prenom']?></h1>
-<p>Voici, ci dessous les frais vous concernant:</p>
-<table class="table">
-<thead>
-  <tr>
-    <th scope="col">Etat</th>
-    <th scope="col">Jour de création</th>
-    <th scope="col">Nombre de justificatifs</th>
-    <th scope="col">Montant valide</th>
-    <th scope="col">Date de modification</th>
-  </tr>
-</thead>
-<tbody>
-    <?php foreach($mesFrais as $m){ ?>
+<div class="container">
+    <h1>Bienvenue <?php echo $_SESSION['nom']." " .$_SESSION['prenom']?></h1>
+    <p>Voici, ci dessous les frais vous concernant:</p>
+    <table class="table">
+        <thead>
         <tr>
-            <th scope="row"><?php echo $m['lib'] ?></th>
-            <td><?php echo convertDate($m['moisAnnee']) ?></td>
-            <td><?php echo $m['nbJustificatifs'] ?></td>
-            <td><?php echo $m['montantValide'] ?>€</td>
-            <td><?php echo convertDate($m['dateModif']) ?></td>
+            <th scope="col">Etat</th>
+            <th scope="col">Jour de création</th>
+            <th scope="col">Nombre de justificatifs</th>
+            <th scope="col">Montant valide</th>
+            <th scope="col">Date de modification</th>
         </tr>
-    <?php } ?>
-</tbody>
-</table>
+        </thead>
+        <tbody>
+            <?php foreach($mesFrais as $m){ ?>
+                <tr>
+                    <th scope="row"><?php echo $m['lib'] ?></th>
+                    <td><?php echo convertDate($m['moisAnnee']) ?></td>
+                    <td><?php echo $m['nbJustificatifs'] ?></td>
+                    <td><?php echo $m['montantValide'] ?>€</td>
+                    <td><?php echo convertDate($m['dateModif']) ?></td>
+                </tr>
+            <?php } ?>
+        </tbody>
+    </table>
+</div>
