ce0c53ad242d5b354a009c6229504361efebc26c
Abdenour le ROI <abdenour.allaouat2@gmail.com>
2024-03-09 10:36:09 +0100

    correction de laffichage

--- controllers/c_ajouterfrais.php
+++ controllers/c_ajouterfrais.php
@@ -1,17 +1,21 @@
 <?php
 
-
-require 'models/m_ajouterfrais.php';
-
 if(!isset($_SESSION['matricule'])){
     header("Location: index.php?page=connexion");
 }
 
+require 'models/m_ajouterfrais.php';
+
+
+
 
 require 'views/v_ajouterfrais.php';
 
-$ajoutFrais = setFrais($_POST['dateFrais'],$_POST['coutFrais'],$_POST['nomFrais'],$_POST['idFrais']);
-// echo $ajoutFrais;
+if(isset($_POST['dateFrais'])&&isset($_POST['coutFrais'])&&isset($_POST['nomFrais'])&&isset($_POST['idFrais'])){
+    $ajoutFrais = setFrais($_POST['dateFrais'],$_POST['coutFrais'],$_POST['nomFrais'],$_POST['idFrais']);
+    echo '<p style="text-align:center;">Les frais ont bien été ajouté</p>';
+}
+
 
 
 
